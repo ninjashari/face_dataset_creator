34f05d64cb48ee569636b734eb6f767727df70f5
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-01-19 22:48:16 +0530

    uncommented usable code

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from face_detection import detect, label
 dest_dir = "./final-dataset"
 
 # Get base directory input and start listing image list
-# base_dir = image_list.get_base_dir()
-# images_list = image_list.get_image_path_list(base_dir)
+base_dir = image_list.get_base_dir()
+images_list = image_list.get_image_path_list(base_dir)
 
 # Run face detection to get an unlabelled face dataset
-# detect.create_unlabelled_face_dataset(images_list)
+detect.create_unlabelled_face_dataset(images_list)
 
 # Label face images
-# label.label_face()
+label.label_face()
 
 # Create csv file
 rename.rename_files(dest_dir)
